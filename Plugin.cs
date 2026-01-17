19564082240071a5b3873709ab6bf7d19e2eb506
Kamil <lekamilthemage@gmail.com>
2026-01-17 18:46:16 +0100

    Cooking UI: prioritize auto-selecting ingredients with lowest stamina

--- Plugin.cs
+++ Plugin.cs
@@ -33,6 +33,50 @@ public class Plugin : BaseUnityPlugin
         Harmony.CreateAndPatchAll(typeof(Plugin));
     }
 
+    // Cooking UI: when auto-selecting ingredients,
+    // prioritize the ones with less stamina value (average of player & villager values)
+    [HarmonyPatch(typeof(CookingUI), "PullIngredients")]
+    [HarmonyPrefix]
+    static bool PrioritizeLowStaminaIngredients(CookingUI __instance)
+    {
+        var ui = __instance;
+        IngredientOption[] ingredOptions = GetField<IngredientOption[]>(ui, "ingredOptions");
+        Inventory playerInventory = GetField<Inventory>(ui, "playerInventory");
+
+        // For each ingredient the recipe requires
+        foreach (IngredientOption option in ingredOptions)
+		{
+            if (!option.gameObject.activeSelf) continue; // Skip unused ingredient slots
+
+            Logger.LogInfo($"Finding best ingredient for category {option.requiredFoodCategoryType}, item {option.requiredFoodItem?.Label}");
+
+            // Find the valid ingredient in the player's inventory that restores the least stamina
+            float lowestStamina = float.MaxValue;
+            int bestIndex = -1;
+			for (int i = 0; i < playerInventory.Items.Length; i++)
+			{
+                InventoryItem item = playerInventory[i].itemReference;
+				if (item != null && ((option.requiredFoodCategoryType != FoodCategoryType.None && option.requiredFoodCategoryType == item.FoodCategoryType) || option.requiredFoodItem == item))
+				{
+                    float staminaValue = (item.PlayerDietCaloricValue + item.CaloricValue) / 2f; // Use average of player & villager stamina values
+                    if (staminaValue < lowestStamina)
+                    {
+                        Logger.LogInfo($"Found better ingredient for recipe slot: {item.Label} (stamina {staminaValue})");
+                        lowestStamina = staminaValue;
+                        bestIndex = i;
+                    }
+				}
+			}
+            // Select the item
+            if (bestIndex != -1)
+            {
+                option.SelectItemFromPlayerInv(playerInventory[bestIndex].itemReference, bestIndex);
+            }
+		}
+		ui.CheckIfAllIngredSelected();
+        return false; // Skip original method
+    }
+
     // Highlight fish, seeds & critters in collections menu that are unobtained but currently available (ie. in season)
     [HarmonyPatch(typeof(CollectionSlotPanel), "SetupFish")]
     [HarmonyPostfix]
