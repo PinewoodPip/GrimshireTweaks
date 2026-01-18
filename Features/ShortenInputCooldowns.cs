7f3d90e5932a2d445593d8a4e6bec97fb3980e6b
Kamil <lekamilthemage@gmail.com>
2026-01-18 18:20:01 +0100

    Make shorter input cooldown only affect interact/left-click
    Was messing with ex. right-clicking an item stack to pick up only a few items

--- Features/ShortenInputCooldowns.cs
+++ Features/ShortenInputCooldowns.cs
@@ -11,15 +11,17 @@ public static class ShortenInputCooldowns
 {
     static float INPUT_COOLDOWN = 0.05f; // Not 0 just in case there's some edgecase in the game that actually warrants a cooldown (possibly the reason why there is one in the first place)
 
-    // Decrease input cooldown
+    // Decrease input cooldown for UI interact/left-click
     // Main use case is to be able to swap items around in the inventory faster;
     // the default 0.2s cooldown gets in the way often.
-    [HarmonyPatch(typeof(UIController), "Start")]
+    [HarmonyPatch(typeof(UIController), "ProcessInput")]
     [HarmonyPostfix]
     static void ReduceInputCooldown(UIController __instance)
     {
-        // Game never rewrites this field, so it's safe to do just in Start()
-        // movementDelayInterval also exists, but it seems to be for UI sliders only(?), so not very relevant.
-        SetField(__instance, "inputDelayInterval", INPUT_COOLDOWN);
+        if (GetField<bool>(__instance, "interact"))
+        {
+            // movementDelayInterval also exists, but it seems to be for UI sliders only(?), so not very relevant.
+            SetField(__instance, "inputDelayCountDown", INPUT_COOLDOWN);
+        }
     }
 }
