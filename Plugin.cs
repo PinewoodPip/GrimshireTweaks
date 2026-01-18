31d7270700b4e741c0e2b7b8a5929192bb3a8e51
Kamil <lekamilthemage@gmail.com>
2026-01-18 10:00:35 +0100

    Optimize object placement checks

--- Plugin.cs
+++ Plugin.cs
@@ -6,6 +6,7 @@ using HarmonyLib;
 using TMPro;
 using UnityEngine;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace GrimshireTweaks;
 
@@ -17,6 +18,14 @@ public class Plugin : BaseUnityPlugin
     static int LATE_WARNING_HOUR = 20;
     static int LATE_WARNING_MINUTE = 0;
     static Color AVAILABLE_COLLECTION_ITEM_COLOR = new Color(0f, 0f, 0f, 0.8f);
+
+    // Mixins and trackers for SquareBoundsChecker optimizations
+    class SquareBoundsCheckerMixin
+    {
+        public Vector3 previousPosition;
+    }
+    static ConditionalWeakTable<SquareBoundsChecker, SquareBoundsCheckerMixin> squareBoundsCheckerMixinStates = new ConditionalWeakTable<SquareBoundsChecker, SquareBoundsCheckerMixin>();
+    static Collider2D[] squareBoundsCheckerCollisionResults = new Collider2D[1];
 
     // These characters are hardcoded to always have service dialogue.
     static HashSet<string> NPCS_WITH_SERVICE_DIALOGUE = new HashSet<string>
@@ -31,6 +40,70 @@ public class Plugin : BaseUnityPlugin
         Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID} is loaded!");
 
         Harmony.CreateAndPatchAll(typeof(Plugin));
+    }
+
+    // Optimize object placement checks
+    // These normally run every frame and lag quite a lot with larger objects (mostly tree seeds),
+    // partially due to allocations from Physics2D.OverlapCircle()
+    [HarmonyPatch(typeof(SquareBoundsChecker), "Validate")]
+    [HarmonyPrefix]
+    static bool OptimizeSquareBoundsChecker(SquareBoundsChecker __instance)
+    {
+        // Skip updating if the position of the checker hasn't changed
+        var previousState = squareBoundsCheckerMixinStates.GetOrCreateValue(__instance);
+        bool positionChanged = previousState.previousPosition != __instance.transform.position;
+        previousState.previousPosition = __instance.transform.position; // Update pos
+        if (!positionChanged)
+        {
+            return false;
+        }
+
+        // Run original logic, but with collision checks modified
+        // to reduce lag from allocations
+        SquareBoundsChecker _this = __instance;
+        var CheckIfValidSpot = Traverse.Create(_this).Method("CheckIfValidSpot", new object[] { true });
+        Vector3 position = _this.transform.position;
+
+        SetField<float>(__instance, "updateCoolDown", 0.05f);
+        SetField<Vector3>(__instance, "myPosition", position);
+
+        // This check was modified to use the NonAlloc variant
+        // to reduce lag from allocation + subsequent garbage collection
+        int collisionsCount = Physics2D.OverlapCircleNonAlloc(position, 0.25f, squareBoundsCheckerCollisionResults, ~(LayerManager.Instance.PlayerLayer | _this.placeableObjRef.layersToIgnore | LayerManager.Instance.IgnoreLayer));
+        CheckIfValidSpot.GetValue(collisionsCount == 0);
+
+        if (_this.placeableObjRef.placeableOnlyInside && _this.validSpot)
+        {
+            CheckIfValidSpot.GetValue(WeatherSystem.Instance.IsInteriorScene());
+        }
+        if (_this.placeableObjRef.placeableOnlyOutside && _this.validSpot)
+        {
+            CheckIfValidSpot.GetValue(!WeatherSystem.Instance.IsInteriorScene());
+        }
+        if (_this.placeableObjRef.placeableOnlyInWater && _this.validSpot)
+        {
+            CheckIfValidSpot.GetValue(_this.placeableObjRef.tileMapManagerRef.IsValidFishTrapSpot(position));
+        }
+        if (_this.placeableObjRef.placeableOnlyInScene != null && _this.validSpot)
+        {
+            CheckIfValidSpot.GetValue(GameManager.Instance.GetCurrentScene() == _this.placeableObjRef.placeableOnlyInScene);
+        }
+        if (_this.placeableObjRef.isPatherTile && _this.validSpot)
+        {
+            CheckIfValidSpot.GetValue(!_this.placeableObjRef.tileMapManagerRef.IsPatherTile(position));
+        }
+        if (_this.placeableObjRef.isWallItem && _this.validSpot)
+        {
+            // Also modified to use NonAlloc variant
+            collisionsCount = Physics2D.OverlapCircleNonAlloc(position, 0.25f, squareBoundsCheckerCollisionResults, LayerManager.Instance.WallLayer);
+            CheckIfValidSpot.GetValue(collisionsCount > 0);
+        }
+        if (_this.placeableObjRef.isPipe && _this.validSpot)
+        {
+            CheckIfValidSpot.GetValue(!_this.placeableObjRef.tileMapManagerRef.IsTilePipe(position));
+        }
+
+        return false;
     }
 
     // Cooking UI: when auto-selecting ingredients,
@@ -114,7 +187,7 @@ public class Plugin : BaseUnityPlugin
             }
         }
     }
-    
+
     // Show notification when it's getting late in the day (so you don't forget to sleep lmao happened to me once)
     [HarmonyPatch(typeof(TimeCompassUI), "SetTimeText")]
     [HarmonyPrefix]
@@ -181,7 +254,7 @@ public class Plugin : BaseUnityPlugin
             textField.text += $"\n\nComposter Value: {compostValue}";
         }
     }
-    
+
     // Skip splash screen (somewhat)
     [HarmonyPatch(typeof(SplashScreenLoader), "Start")]
     [HarmonyPrefix]
@@ -196,5 +269,23 @@ public class Plugin : BaseUnityPlugin
     {
         var field = Traverse.Create(obj).Field(fieldName);
         return field.GetValue<T>();
+    }
+
+    // Utility method to write private fields.
+    static void SetField<T>(object obj, string fieldName, T value)
+    {
+        var field = Traverse.Create(obj).Field(fieldName);
+        field.SetValue(value);
+    }
+
+    static T CallMethod<T>(object obj, string methodName, params object[] parameters)
+    {
+        var method = Traverse.Create(obj).Method(methodName, parameters);
+        return method.GetValue<T>();
+    }
+    static void CallMethod(object obj, string methodName, params object[] parameters)
+    {
+        var method = Traverse.Create(obj).Method(methodName, parameters);
+        method.GetValue();
     }
 }
